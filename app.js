b43c1ec5e06391f13c49cd65a5f138dd44640856
Cody Walenciak <codywalenciak@gmail.com>
2024-12-09 16:58:33 -0600

    Add session config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const Knex = require('knex');
 const knexConfig = require('./knexfile');
 const { Model, ForeignKeyValidationError, ValidationError } = require('objection');
@@ -11,7 +12,11 @@ Model.knex(knex);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    maxAge: null
+}))
 
 const routes = require('./server');
 app.use('/', routes);
