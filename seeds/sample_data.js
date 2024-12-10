e67e91ff499862f6f1a60fc2cf8d7a65f1c133f3
Cody Walenciak <codywalenciak@gmail.com>
2024-12-10 16:08:30 -0600

    Add additional sample data

--- seeds/sample_data.js
+++ seeds/sample_data.js
@@ -25,7 +25,8 @@ exports.seed = async function(knex) {
     {name: 'salt'},
     {name: 'sugar'},
     {name: 'celery'},
-    {name: 'carrot'}
+    {name: 'carrot'},
+    {name: 'butter'}
     
   ]);
   await knex('regions').del()
@@ -33,13 +34,15 @@ exports.seed = async function(knex) {
     {name: 'Italy'},
     {name: 'Korea'},
     {name: 'Mexico'},
-    {name: 'Canada'}
+    {name: 'Canada'},
+    {name: 'France'}
   ]);
   await knex('recipes').del()
   await knex('recipes').insert([
     {name: 'Marinara', directions: 'make some sauce', regionId: 1},
-    {name: 'Salsa Roja', directions: 'Deseed and toast smoked chile, char other vegetables, blend chiles and vegetables with oil and lime jice, add salt', regionId: 3},
-
+    {name: 'Salsa Roja', directions: 'Deseed and toast smoked chile, char other vegetables, blend chiles and vegetables with oil and lime juice, add salt', regionId: 3},
+    {name: 'Bolognese', directions: 'Prepare marinara sauce. Chop vegetables and saute. Add meat to vegetables and let cook. Add marinara, and let simmer.', regionId: 1},
+    {name: 'Bechamel', directions: 'Melt butter, add flour and whisk until smooth, and cook until lightly browned. Add milk and whisk, then let simmer. Add salt to taste.', regionId: 5}
   ]);
   await knex('recipes_ingredients').del()
   await knex('recipes_ingredients').insert([
@@ -53,5 +56,17 @@ exports.seed = async function(knex) {
     {recipeId: 2, ingredientId: 7},
     {recipeId: 2, ingredientId: 10},
     {recipeId: 2, ingredientId: 11},
+    {recipeId: 3, ingredientId: 1},
+    {recipeId: 3, ingredientId: 2},
+    {recipeId: 3, ingredientId: 3},
+    {recipeId: 3, ingredientId: 4},
+    {recipeId: 3, ingredientId: 9},
+    {recipeId: 3, ingredientId: 10},
+    {recipeId: 3, ingredientId: 17},
+    {recipeId: 3, ingredientId: 18},
+    {recipeId: 4, ingredientId: 19},
+    {recipeId: 4, ingredientId: 14},
+    {recipeId: 4, ingredientId: 13},
+    {recipeId: 4, ingredientId: 15},
   ]);
 };
